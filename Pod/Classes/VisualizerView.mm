43044dfc6bfeca4b78e76a1af8be14d806d5bb7c
Nick Chavez <nchavez324@yahoo.com>
2014-11-04 16:09:40 -0500

    iOS 7 compatible

--- Pod/Classes/VisualizerView.mm
+++ Pod/Classes/VisualizerView.mm
@@ -13,12 +13,12 @@
 #import "NCToolBelt.h"
 
 @implementation VisualizerView {
-  CAEmitterLayer *emitterLayer;
-  MeterTable meterTable;
+    CAEmitterLayer *emitterLayer;
+    MeterTable meterTable;
 }
 
 + (Class)layerClass {
-  return [CAEmitterLayer class];
+    return [CAEmitterLayer class];
 }
 
 + (UIColor *)defaultColor {
@@ -33,60 +33,62 @@
 
 - (instancetype)initWithFrame:(CGRect)frame color:(UIColor *)color {
     
-  self = [super initWithFrame:frame];
-  if (self) {
-    [self setBackgroundColor:[UIColor blackColor]];
-    emitterLayer = (CAEmitterLayer *)self.layer;
-    
-    CGFloat width = MAX(frame.size.width, frame.size.height);
-    CGFloat height = MIN(frame.size.width, frame.size.height);
-    emitterLayer.emitterPosition = CGPointMake(width/2, height/2);
-    emitterLayer.emitterSize = CGSizeMake(width-80, 60);
-    emitterLayer.emitterShape = kCAEmitterLayerRectangle;
-    emitterLayer.renderMode = kCAEmitterLayerAdditive;
-    
-    CAEmitterCell *cell = [CAEmitterCell emitterCell];
-    cell.name = @"cell";
-    
-    CAEmitterCell *childCell = [CAEmitterCell emitterCell];
-    childCell.name = @"childCell";
-    childCell.lifetime = 1.0f / 60.0f;
-    childCell.birthRate = 60.0f;
-    childCell.velocity = 0.0f;
-    
-    UIImage *p = [UIImage imageNamed:@"particleTexture" inBundle:[[NCToolBelt sharedInstance] internalBundle] compatibleWithTraitCollection:nil];
-      childCell.contents = (id)p.CGImage;
-      
-    cell.emitterCells = @[childCell];
-    
-    cell.color = [color CGColor];
-    cell.redRange = 0.46f;
-    cell.greenRange = 0.49f;
-    cell.blueRange = 0.67f;
-    cell.alphaRange = 0.55f;
-    
-    cell.redSpeed = 0.11f;
-    cell.greenSpeed = 0.07f;
-    cell.blueSpeed = -0.25f;
-    cell.alphaSpeed = 0.15f;
-    
-    cell.scale = 0.5f;
-    cell.scaleRange = 0.5f;
-    
-    cell.lifetime = 1.0f;
-    cell.lifetimeRange = .25f;
-    cell.birthRate = 80;
-    
-    cell.velocity = 100.0f;
-    cell.velocityRange = 300.0f;
-    cell.emissionRange = M_PI * 2;
-    
-    emitterLayer.emitterCells = @[cell];
-
-    CADisplayLink *dpLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
-    [dpLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
-  }
-  return self;
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setBackgroundColor:[UIColor blackColor]];
+        emitterLayer = (CAEmitterLayer *)self.layer;
+        
+        CGFloat width = MAX(frame.size.width, frame.size.height);
+        CGFloat height = MIN(frame.size.width, frame.size.height);
+        emitterLayer.emitterPosition = CGPointMake(width/2, height/2);
+        emitterLayer.emitterSize = CGSizeMake(width-80, 60);
+        emitterLayer.emitterShape = kCAEmitterLayerRectangle;
+        emitterLayer.renderMode = kCAEmitterLayerAdditive;
+        
+        CAEmitterCell *cell = [CAEmitterCell emitterCell];
+        cell.name = @"cell";
+        
+        CAEmitterCell *childCell = [CAEmitterCell emitterCell];
+        childCell.name = @"childCell";
+        childCell.lifetime = 1.0f / 60.0f;
+        childCell.birthRate = 60.0f;
+        childCell.velocity = 0.0f;
+        
+        NSString *imagePath = [[NCToolBelt sharedInstance].internalBundle pathForResource:@"particleTexture" ofType:@"png"];
+        
+        UIImage *p = [UIImage imageWithContentsOfFile:imagePath];
+        childCell.contents = (id)p.CGImage;
+        
+        cell.emitterCells = @[childCell];
+        
+        cell.color = [color CGColor];
+        cell.redRange = 0.46f;
+        cell.greenRange = 0.49f;
+        cell.blueRange = 0.67f;
+        cell.alphaRange = 0.55f;
+        
+        cell.redSpeed = 0.11f;
+        cell.greenSpeed = 0.07f;
+        cell.blueSpeed = -0.25f;
+        cell.alphaSpeed = 0.15f;
+        
+        cell.scale = 0.5f;
+        cell.scaleRange = 0.5f;
+        
+        cell.lifetime = 1.0f;
+        cell.lifetimeRange = .25f;
+        cell.birthRate = 80;
+        
+        cell.velocity = 100.0f;
+        cell.velocityRange = 300.0f;
+        cell.emissionRange = M_PI * 2;
+        
+        emitterLayer.emitterCells = @[cell];
+        
+        CADisplayLink *dpLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
+        [dpLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    }
+    return self;
 }
 
 - (void)update {
